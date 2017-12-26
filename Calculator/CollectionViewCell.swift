6c676b31eac1d89d607596130442f08625b72027
xiongyc <971881086@qq.com>
2017-12-26 21:33:51 +0800

    init

---
+++ Calculator/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Calculator
+//
+//  Created by YC X on 2017/12/24.
+//  Copyright © 2017年 YC X. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
